a482f971821b8a33e7f1c7c1ec283204bbf60554
HarshaniBandara <harshani.19@cse.mrt.ac.lk>
2022-09-23 11:44:19 +0530

    login page and style sheets

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import './Login.css'
+import './login.css'
 import logo from'../assets/logo.png'
 import welcomeimg from '../assets/doctor.png'
 function login () {
