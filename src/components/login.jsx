07cf5b73b410c338a3034a2df8138dd41c90e181
HarshaniBandara <harshani.19@cse.mrt.ac.lk>
2022-09-23 08:14:54 +0530

    login page

---
+++ src/components/login.jsx
@@ -0,0 +1,44 @@
+import React,{useState} from 'react'
+import './Login.css'
+import logo from'../assets/logo.png'
+import welcomeimg from '../assets/doctor.png'
+function login () {
+    // const handlesubmit-(event)->{
+    //     event.preventDefault();
+    // }
+  return (
+    
+    <div className="main-login">
+       <br></br>
+        {/* <link >Register</link> */}
+        <div className="login-container">
+            <div className="left-side">
+                <div className="img-class">
+                    <img src={logo} alt="" />
+                </div>
+                <form >
+                    <label for="email">Email</label>
+                        <input type="email" placeholder='Enter your email..'id='email' />
+                    <label for="password">Password</label>
+                        <input type="password" placeholder='Enter password'id='password'/>
+                    <button type='submit'>Submit</button>
+                </form>
+                <div className="footer">
+                    <h4>Dont' have an account.. <link  href="" /></h4>
+                </div>
+            </div>
+
+            <div className="right-side">
+                <div className="welcome-note">
+                    <h3>........Welcome Back........</h3>
+                </div>
+                <div className="welcome-img">
+                    <img src={welcomeimg} id='wel-img-id'alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default login;
