05dab303ab547048ea1a4ec415bace5ad8de517c
HarshaniBandara <harshani.19@cse.mrt.ac.lk>
2022-09-30 17:19:43 +0530

    Doctor Dashboard created

---
+++ src/components/doctorDashboard.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import Footer from './footer'
+import '../CSS/doctorDashboard.css'
+import { Link } from 'react-router-dom';
+const DoctorDashboard = () => {
+  return (
+    <div className='dashboard-container'>
+       {/* fffffffffffffffff */}
+       <section class='dashboard'>
+            <h1>DASHBOARD</h1>
+            <p></p>
+            <div class='row'>
+                <div class="testimanial-col">
+                    <Link  to='./roster' style={{textDecoration:"none", alignItems:'center',marginLeft:"20px" ,}}>
+                    
+                        <img src={require("../assets/roster-icon.jpg")} alt=""></img>
+                        <div>
+                            <p>
+                                MY ROSTER
+                            </p>
+                            <h3></h3>
+                            <h6></h6>
+                        </div>
+                    </Link>
+                </div>
+                       
+                <div class="testimanial-col">
+                    <Link  to='./wardRoster' style={{textDecoration:"none", alignItems:'center',marginLeft:"20px" ,}}>
+                    
+                        <img src={require("../assets/ward-roster.webp")} alt=""></img>
+                        <div>
+                            <p>
+                                WARD ROSTER
+                            </p>
+                            
+                        </div>
+                    </Link>
+                </div>
+                
+            </div>
+            <div class='row'>
+                                      
+                        
+                <div class="testimanial-col">
+                <Link  to='./doctorProfile' style={{textDecoration:"none", alignItems:'center',marginLeft:"20px" ,}}>
+                    <img src={require("../assets/profile-icon.png")} alt=""></img>
+                    <div>
+                        <p>
+                            PROFILE
+                        </p>
+                        
+                    </div>
+                    </Link> 
+                </div>
+                
+                <div class="testimanial-col">
+                <Link  to='./shiftRequest' style={{textDecoration:"none", alignItems:'center',marginLeft:"20px" ,}}>
+                
+                    <img src={require("../assets/doctor.png")} alt=""></img>
+                    <div>
+                        <p active={true} >
+                            SHIFT REQUEST
+                        </p>
+                       
+                    </div>
+                    </Link>
+                </div>
+            </div>
+            {/* <div className="row">
+                <div class="testimanial-col">
+                    <Link  to='./shiftRequest' style={{textDecoration:"none", alignItems:'center',marginLeft:"20px" ,}}>
+                    
+                        <img src={require("../assets/doctor.png")} alt=""></img>
+                        <div>
+                            <p active={true} >
+                                NOTIFICATION
+                            </p>
+                        
+                        </div>
+                        </Link>
+                </div>
+            </div> */}
+        </section>
+        <br />
+        <br />
+       
+    </div>
+  )
+}
+
+export default DoctorDashboard
