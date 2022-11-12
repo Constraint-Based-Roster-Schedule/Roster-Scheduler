9f34cd43f839320f6a604498080e7c56482ee65f
Sakuni99 <sakunihiru@gmail.com>
2022-11-12 07:25:00 +0530

    components seperate in ward roster

--- src/view/wardRoster.js
+++ src/view/wardRoster.js
@@ -1,69 +1,30 @@
-import React from 'react'
+import React from 'react';
+import {useState,useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import '../CSS/wardRoster.css';
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 import WardRosterComponent from '../components/wardRosterComponent';
-import Axios from "axios";
 import Box from '@mui/material/Box';
+import Axios from "axios";
 
 
 function  WardRoster() {
-
     const [shiftNames,setShiftNames]=useState([]);
-    const [docID,setDocID]=useState("");
-
-    const [finalShifts,setFinalShifts]=useState([]);
 
     useEffect(()=>{
-        fetchIndividualRoster();       
+        fetchShiftnames();
     },[])
 
-    const fetchIndividualRoster=async()=>{
-        
-        const monthNames = ["january", "february", "march", "april", "may", "june",
-                            "july", "august", "september", "october", "november", "december"
-                            ];
-        const current_month=new Date().getMonth();
-        const required_months=[]
-        required_months.push(monthNames[current_month-2]);
-        required_months.push(monthNames[current_month-1]);
-        required_months.push(monthNames[current_month]);
-        required_months.push(monthNames[current_month+1]);
-        
-        console.log(required_months); 
 
-        await Axios.get("http://localhost:5000/user/doctor/getRosterObject",{
-            params:{"month":"november","year":"2022","months":required_months}
+    const fetchShiftnames=async()=>{
+        await Axios.get("http://localhost:5000/user/doctor/getShiftNames",{
+            params:{"month":"november","year":"2022"}
         }).then((res) => {
-        const myShifts=res.data.myShifts;
-        const shiftNames=res.data.shiftNames
-        console.log(shiftNames)
-        setShiftNames(shiftNames)
-        const data_to_send=[]
-        myShifts.forEach((mon,month_index)=>{
-            mon.forEach((day,date)=>{
-                day.forEach((shift,index)=>{
-                    const shift_string=shift.join(" , ")
-                    //console.log(shift_string)
-                    const shift_detail={
-                        title: shift_string,
-                        startDate: new Date(2022, 10+month_index-2, date+1, shiftNames[index][2][0][0], shiftNames[index][2][0][1]),
-                        endDate: new Date(2022, 10+month_index-2,date+1, shiftNames[index][2][1][0], shiftNames[index][2][1][1]),
-                        color:shiftNames[index][1],
-                    }
-                    data_to_send.push(shift_detail)
-                        
-                })
 
-            });
-        })
-        
-        setFinalShifts(data_to_send);
+            setShiftNames(res.data.shiftNames)
         })
     }
-
 
     return (
         <>
@@ -81,7 +42,7 @@ function  WardRoster() {
                     }
                 </div>
             </div>
-            <WardRosterComponent appointments={finalShifts}/>
+            <WardRosterComponent/>
         </>        
     )
 }
