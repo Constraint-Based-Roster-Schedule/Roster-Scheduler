b5b422e1b3459be4ced2d40120193485443c98a0
Sakuni99 <sakunihiru@gmail.com>
2022-11-20 20:29:03 +0530

    deployement commits

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/js/bootstrap";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import "react-bootstrap/dist/react-bootstrap.min";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,7 +17,7 @@ root.render(
        
   </React.StrictMode>
 );
-
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
